14318f9fcaab1cc5cb0373ef73efeed02697cb48
ti00qq <ti00qq@gmail.com>
2018-11-05 13:05:48 +0000

    have

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -7,6 +7,18 @@ use \App\Item;
 
 class ItemsController extends Controller
 {
+    public function show($id)
+    {
+      $item = Item::find($id);
+      $want_users = $item->want_users;
+      $want_users = $item->have_users;
+
+      return view('items.show', [
+          'item' => $item,
+          'want_users' => $want_users,
+          'have_users' => $have_users,
+      ]);
+    }
 
     public function create()
     {

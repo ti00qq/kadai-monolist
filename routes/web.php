b3d504b19ba05ed757c780aaa189bf345d19d526
ti00qq <ti00qq@gmail.com>
2018-11-06 14:43:17 +0000

    ranking

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,9 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
+// ランキング
+Route::get('ranking/want', 'RankingController@want')->name('ranking.want');
+
 //ログイン後
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('items', 'ItemsController', ['only' => ['create', 'show']]);
